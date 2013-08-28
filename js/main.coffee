ab1f9fd734690abf8ffd645a19765bb1bf261472
Ilja <pman89@ya.ru>
2013-08-28 11:36:06 +0400

    map script test

--- js/main.coffee
+++ js/main.coffee
@@ -1,7 +1,38 @@
-ymaps.ready ->
-  myMap = new ymaps.Map("YMapsID",
-    center: [60.705288, 28.762311]
-    zoom: 14
-  )
-  myMap.controls.add "zoomControl"
-  myMap.controls.add new ymaps.control.ScaleLine()
+$(document).ready ->
+  ymaps.ready ->
+    addMenuItem = (group, map, menuContainer) ->
+      $("""<li><ul id="menu#{group.properties.get("description")}"></ul></li>""").appendTo menuContainer
+    
+    addMenuItem2 = (group, map, container) ->
+      img = ""
+      unless group.getLength() is 0
+        img = group.get(0).options.get("iconImageHref")
+      else
+        img = "http://allvbg.ru/static/allvbg/img/transparent.png"
+      item = $("<a class='title_sub' href='#'><img src='#{img}' alt=''/>#{group.properties.get("name")}</a>")
+      item.bind("click", ->
+        link = $(this)
+        if link.hasClass("active_sub")
+          map.geoObjects.remove group
+        else
+          map.geoObjects.add group
+        link.toggleClass "active_sub"
+        false
+      ).appendTo $("<li></li>").appendTo($(container))
+
+    myMap = new ymaps.Map("YMapsID",
+      center: [28.762311, 60.705288]
+      zoom: 14
+    )
+    myMap.controls.add "zoomControl"
+    #myMap.controls.add new ymaps.control.ScaleLine()
+
+    ymaps.geoXml.load("http://allvbg.ru/main_map.xml").then ((res) ->
+      res.geoObjects.each (item) ->
+        addMenuItem item, myMap, $("#menu")
+        item.each (item2) ->
+          cnt = "#menu" + item.properties.get("description")
+          addMenuItem2 item2, myMap, cnt
+      $('#scrollbar1').tinyscrollbar()
+    ), (error) ->
+      alert "При загрузке YMapsMl-файла произошла ошибка: " + error
--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
 (function() {
-  ymaps.ready(function() {
-    var myMap;
-    myMap = new ymaps.Map("YMapsID", {
-      center: [60.705288, 28.762311],
-      zoom: 14
+  $(document).ready(function() {
+    return ymaps.ready(function() {
+      var addMenuItem, addMenuItem2, myMap;
+      addMenuItem = function(group, map, menuContainer) {
+        return $("<li><ul id=\"menu" + (group.properties.get("description")) + "\"></ul></li>").appendTo(menuContainer);
+      };
+      addMenuItem2 = function(group, map, container) {
+        var img, item;
+        img = "";
+        if (group.getLength() !== 0) {
+          img = group.get(0).options.get("iconImageHref");
+        } else {
+          img = "http://allvbg.ru/static/allvbg/img/transparent.png";
+        }
+        item = $("<a class='title_sub' href='#'><img src='" + img + "' alt=''/>" + (group.properties.get("name")) + "</a>");
+        return item.bind("click", function() {
+          var link;
+          link = $(this);
+          if (link.hasClass("active_sub")) {
+            map.geoObjects.remove(group);
+          } else {
+            map.geoObjects.add(group);
+          }
+          link.toggleClass("active_sub");
+          return false;
+        }).appendTo($("<li></li>").appendTo($(container)));
+      };
+      myMap = new ymaps.Map("YMapsID", {
+        center: [28.762311, 60.705288],
+        zoom: 14
+      });
+      myMap.controls.add("zoomControl");
+      return ymaps.geoXml.load("http://allvbg.ru/main_map.xml").then((function(res) {
+        res.geoObjects.each(function(item) {
+          addMenuItem(item, myMap, $("#menu"));
+          return item.each(function(item2) {
+            var cnt;
+            cnt = "#menu" + item.properties.get("description");
+            return addMenuItem2(item2, myMap, cnt);
+          });
+        });
+        return $('#scrollbar1').tinyscrollbar();
+      }), function(error) {
+        return alert("При загрузке YMapsMl-файла произошла ошибка: " + error);
+      });
     });
-    myMap.controls.add("zoomControl");
-    return myMap.controls.add(new ymaps.control.ScaleLine());
   });
 
 }).call(this);
